05f4db16e76417158d60d09616f5956b19bec0ac
Виктор Тузовский <aletissarichard@gmail.com>
2022-01-26 22:23:39 +0300

    Added objects

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -8,6 +8,7 @@ User = get_user_model()
 
 
 class Post(models.Model):
+    objects = None
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
