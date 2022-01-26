951da25655b7a8bb2160168246ebfefb0a6d7587
Виктор Тузовский <aletissarichard@gmail.com>
2022-01-26 22:17:14 +0300

    Tests passed successfully

---
+++ yatube/posts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        'title': 'Последние обновления на сайте',
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': f'Записи сообщества {group.title}',
+    }
+
+    return render(request, 'posts/group_list.html', context)
