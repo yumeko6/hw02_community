41c62015234fd8674cdd55a8fa1cafbb22420da3
Виктор Тузовский <aletissarichard@gmail.com>
2022-02-02 16:47:59 +0300

    Added paths for password change

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib.auth.views import LogoutView, LoginView, PasswordChangeView
 from django.urls import path
 from . import views
 
@@ -17,4 +17,14 @@ urlpatterns = [
         LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
+    path('password_change/',
+         PasswordChangeView.as_view(
+             template_name='users/password_change_form.html'),
+         name='password_change'
+         ),
+    path('password_change/done/',
+         PasswordChangeView.as_view(
+             template_name='users/password_change_done.html'),
+         name='password_change_done'
+         ),
 ]
