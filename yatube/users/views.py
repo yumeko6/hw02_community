8ee1c73aa9a503e117eefabd9c79473650f6cae5
Виктор Тузовский <aletissarichard@gmail.com>
2022-02-02 16:48:22 +0300

    Added view for password change

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,9 +1,15 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, FormView
 from django.urls import reverse_lazy
-from .forms import CreationForm
+from .forms import CreationForm, PasswordForm
 
 
 class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy('posts:index')
     template_name = 'users/signup.html'
+
+
+class PasswordChange(FormView):
+    form_class = PasswordForm
+    success_url = reverse_lazy('users:password_change_done')
+    template_name = 'users/password_change_form.html'
