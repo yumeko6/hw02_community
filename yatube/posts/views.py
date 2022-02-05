0a69898246cb024b8a1b69ccec3d985967a7d927
Виктор Тузовский <aletissarichard@gmail.com>
2022-02-05 14:14:57 +0300

    Added Paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,15 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
 
-LAST_POSTS = 10
-
-
 def index(request):
-    posts = Post.objects.all()[:LAST_POSTS]
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
         'title': 'Последние обновления на сайте',
     }
     return render(request, 'posts/index.html', context)
@@ -16,10 +17,13 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).all()[:LAST_POSTS]
+    posts = Post.objects.filter(group=group).all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
         'title': f'Записи сообщества {group.title}',
     }
 
